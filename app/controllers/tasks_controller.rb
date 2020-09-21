2a8af719ec2fc3b595c8cf15db9d4502b02921f2
Ray <sttrppasasa@gmail.com>
2020-09-21 15:56:23 +0000

    add status

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,70 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:edit, :show, :update, :destroy]
+  
+  
+  def index
+    
+    @tasks = Task.all
+  
+  end  
+  
+  def create
+    
+    @task = Task.new(task_params)
+    
+    if @task.save
+      flash[:succes] = 'Tasklistは正常に更新されました'
+      redirect_to @task
+      
+    else
+      flash.now[:danger] = 'Tasklisは正常に更新されませんでした'
+     render :new
+    end
+  end  
+  
+  def new
+    
+    @task = Task.new
+  
+  end  
+  
+  def edit
+  end  
+  
+  def show
+  end  
+  
+  def update
+    
+    
+      if @task.update(task_params)
+        flash[:succes] = 'タスクは正常に更新されました'
+        redirect_to @task
+
+      else
+        flash.now[:danger] = 'タスクは正常に更新されませんでした'
+        render :edit
+      end
+  
+  end  
+  
+  def destroy
+    
+    @task.destroy
+    
+    flash[:succes] = 'タスクが削除されました'
+    redirect_to root_url
+  end
+  
+  
+  private
+  
+  def set_task
+    @task = Task.find(params[:id])
+  end
+  
+  def task_params
+    params.require(:task).permit(:content, :status)
+  end
+  
+end
